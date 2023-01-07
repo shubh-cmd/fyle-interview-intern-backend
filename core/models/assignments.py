bed79d01658cdc9df99c80cdb4ef958d501f40d6
shubh-cmd <krshubham878@gmail.com>
2023-01-07 14:43:39 +0530

    Task complete

--- core/models/assignments.py
+++ core/models/assignments.py
@@ -5,6 +5,7 @@ from core.libs import helpers, assertions
 from core.models.teachers import Teacher
 from core.models.students import Student
 from sqlalchemy.types import Enum as BaseEnum
+from marshmallow import ValidationError
 
 
 class GradeEnum(str, enum.Enum):
@@ -62,9 +63,11 @@ class Assignment(db.Model):
     @classmethod
     def submit(cls, _id, teacher_id, principal: Principal):
         assignment = Assignment.get_by_id(_id)
+        print(_id)
         assertions.assert_found(assignment, 'No assignment with this id was found')
         assertions.assert_valid(assignment.student_id == principal.student_id, 'This assignment belongs to some other student')
         assertions.assert_valid(assignment.content is not None, 'assignment with empty content cannot be submitted')
+        assertions.assert_valid(assignment.state == AssignmentStateEnum.DRAFT,'only a draft assignment can be submitted')
 
         assignment.teacher_id = teacher_id
         assignment.state = AssignmentStateEnum.SUBMITTED
@@ -75,3 +78,34 @@ class Assignment(db.Model):
     @classmethod
     def get_assignments_by_student(cls, student_id):
         return cls.filter(cls.student_id == student_id).all()
+
+    @classmethod
+    def assign_grade(cls, _id, grade, principal : Principal):
+        assignment = Assignment.get_by_id(_id)
+        
+        # print(assignment.content)
+        assertions.assert_found(assignment, 'No assignment with this id was found')
+        assertions.assert_valid(assignment.teacher_id == principal.teacher_id, 'This assignment belongs to some other teacher')
+        assertions.assert_valid(assignment.grade is None, 'assignment is already graded')
+
+        if grade == 'A':
+            assignment.grade = GradeEnum.A
+        elif grade == 'B':
+            assignment.grade = GradeEnum.B
+        elif grade == 'C':
+            assignment.grade = GradeEnum.C
+        elif grade == 'D':
+            assignment.grade = GradeEnum.D
+        else:
+            raise ValidationError(
+                "Invalid grade"
+            )
+
+        assignment.state = AssignmentStateEnum.GRADED
+        db.session.flush()
+
+        return assignment
+
+    @classmethod
+    def get_assignments_submitted_to_teacher(cls, teacher_id):
+        return cls.filter(cls.teacher_id == teacher_id).all()
